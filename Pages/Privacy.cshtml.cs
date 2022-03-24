cf97f967417914d2edffe8f1184d28a46e1c06be
Lucas Stephenson <lstephenson@cegep-heritage.qc.ca>
2022-03-24 09:48:54 -0400

    init commit

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razor_ef_2022.Pages;
+
+public class PrivacyModel : PageModel
+{
+
+	public string Message { get; private set; } = "Message!";
+	public string? Greeting { get; private set; }
+	private readonly ILogger<PrivacyModel> _logger;
+
+	public PrivacyModel(ILogger<PrivacyModel> logger)
+	{
+		_logger = logger;
+	}
+
+	public void OnGet()
+	{
+		Message += $" the time is {DateTime.Now}";
+		Greeting = $"<a href=\"https://google.ca\">Google</a>";
+	}
+
+
+}
+
